11bf575120c277db622ea7ceaf1355fda6c9f855
Franco Caraffa <francocaraffa2005@gmail.com>
2023-06-13 22:48:32 -0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,35 @@
+const videoTitle = document.querySelector("#video-title")
+const videoImg = document.querySelector("#video-img");
+const videoA = document.querySelector("#videoA");
+const API = "https://youtube-v31.p.rapidapi.com/search?channelId=UCam8T03EOFBsNdR0thrFHdQ&part=snippet%2Cid&order=date&maxResults=3";
+
+const fetchData = () =>{
+    const headers = {
+      "X-RapidAPI-Key": "567b71fa43mshadd7428efa65ae3p14d840jsn88e59dc89532",
+      "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+    };
+    return fetch(API, {headers});
+}
+
+
+fetchData()
+.then(data => data.json())
+.then(info => modificarDom(info))
+.then(newInfo => displayData(newInfo))
+.catch(error => console.log(error))
+
+function modificarDom(info){
+  console.log(info)
+  const infoo = {
+    info1: info.items[0].snippet.thumbnails.high.url,
+    info2: info.items[0].snippet.title,
+    videoID: info.items[0].id.videoId
+  }
+  return infoo
+}
+
+function displayData(info){
+  videoTitle.innerHTML = `<p>${info.info2}</p>`;
+  videoImg.innerHTML = `<img src="${info.info1}"></img>`;
+  videoA.setAttribute("href", `https://www.youtube.com/watch?v=${info.videoID}`);
+}
